6deae67e38a3fc331143a88c3ed23c09ff17e4ca
Ihor Kliushnyk <igorator746@gmail.com>
2024-03-28 01:07:31 +0200

    Cookies banner redesign

--- components/CookiesConcentBanner.tsx
+++ components/CookiesConcentBanner.tsx
@@ -23,30 +23,22 @@ export const CookieConsent = () => {
 
     return (
         <div
-            className={`fixed inset-0 bg-black bg-opacity-70 transition-opacity duration-200 ${showConsent ? 'pointer-events-none opacity-0' : 'opacity-100 '}`}
+            className={`fixed flex max-w-[500px] flex-col items-end gap-[30px] bg-gray-100 px-4 py-4 transition-opacity duration-200 mobile:bottom-0 mobile:flex-col mobile:shadow-none desktop:bottom-[20px] desktop:right-[20px] desktop:rounded-[5px] desktop:shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)] ${showConsent ? 'translate-y-16 opacity-0 ' : 'translate-y-0 opacity-100'}`}
         >
-            <div
-                className={`fixed bottom-0 left-0 right-0 flex items-center justify-between gap-[30px] bg-gray-100 px-4 py-8 transition-opacity duration-200 mobile:flex-col desktop:flex-row ${showConsent ? 'translate-y-16 opacity-0 ' : 'translate-y-0 opacity-100'}`}
+            <span className='w-fit text-pretty font-secondary text-black'>
+                This website uses cookies to improve user experience. By using
+                our website you consent to all cookies in accordance with our{' '}
+                <ScrollToTopLink href={routes.privacy} className='underline'>
+                    Cookie Policy
+                </ScrollToTopLink>
+                .
+            </span>
+            <button
+                className='w-fit rounded bg-[#212121] px-8 py-2 font-secondary text-white duration-150 ease-out hover:opacity-70'
+                onClick={acceptCookie}
             >
-                <span className='text-center font-secondary text-base text-black mobile:mr-0 mobile:text-balance desktop:mr-16'>
-                    This website uses cookies to improve user experience. By
-                    using our website you consent to all cookies in accordance
-                    with our{' '}
-                    <ScrollToTopLink
-                        href={routes.privacy}
-                        className='underline'
-                    >
-                        Cookie Policy
-                    </ScrollToTopLink>
-                    .
-                </span>
-                <button
-                    className='rounded bg-[#212121] px-8 py-2 font-secondary text-white'
-                    onClick={acceptCookie}
-                >
-                    Accept
-                </button>
-            </div>
+                Accept
+            </button>
         </div>
     )
 }
