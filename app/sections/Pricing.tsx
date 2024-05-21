3ca7a2387617eabf9af5974541ac82a9ebdf5cf5
Ihor Kliushnyk <igorator746@gmail.com>
2024-05-21 18:22:29 +0300

    pricing mobile adaptation

--- app/sections/Pricing.tsx
+++ app/sections/Pricing.tsx
@@ -8,7 +8,7 @@ export const Pricing: React.FC<{
 }> = ({ id, pricingCardsData }) => {
     return (
         <Section id={id}>
-            <div className='grid w-full grid-cols-3 gap-[24px]'>
+            <div className='mon grid w-full grid-cols-3 gap-[24px] mobile:grid-cols-1 mobile:gap-[36px]'>
                 {pricingCardsData.map((card) => (
                     <PricingCard
                         key={card.id}
